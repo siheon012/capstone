e87ab2dad8080f9634fe2139f96858b74a31ff43
siheon012 <sihunyy@gmail.com>
2026-01-13 01:14:50 +0900

    redeploy backend

--- back/apps/api/services/__init__.py
+++ back/apps/api/services/__init__.py
@@ -1,14 +1,16 @@
 # S3 및 기타 서비스 관련 모듈들
 
 from .video_service import VideoService, get_video_service
-from .s3_service import S3VideoUploadService
-from .sqs_service import SQSService
+from .s3_service import S3VideoUploadService, s3_service
+from .sqs_service import SQSVideoProcessingService, sqs_service
 from .auth_service import jwt_required
 
 __all__ = [
     'VideoService',
     'get_video_service',
     'S3VideoUploadService',
-    'SQSService',
+    's3_service',
+    'SQSVideoProcessingService',
+    'sqs_service',
     'jwt_required',
 ]
